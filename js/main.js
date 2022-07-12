584c750247934cc4447ae7f11bd1a068dfba6d86
Saulo <saulo@saulo.com>
2022-07-12 13:24:38 -0300

    Alterações no arquivo main

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
-function calculoComprimento(inputNum,diferenca){
+function calculoComprimento(inputNum, diferenca){
 
-    const moduloDiferenca = diferenca < 0 ? 
-    () => { return inputNum / Math.pow(10, diferenca * -1); } 
-    : 
-    () => { return inputNum * Math.pow(10, diferenca); }
-    return moduloDiferenca();
+    inputNum = (inputNum) == false? 1 : inputNum;
+    diferenca = (diferenca) == false? 0 : diferenca;
+    return Number(`${inputNum}E${diferenca}`);
 }
 
 function converter() {
-    const $input = document.getElementById('in');
-    const $output = document.getElementById('out');
+    
     const $insel = document.getElementById('inselect');
     const $outsel = document.getElementById('outselect');
-    $output.innerText = "Resultado: " + calc($input.value, $insel.selectedIndex - $outsel.selectedIndex);
+    document.querySelector('#out').innerText = "Resultado: " + 
+    calculoComprimento(document.querySelector('#in').value, $insel.selectedIndex - $outsel.selectedIndex);
     return;
 }
+
+function createSelectElement(id = 'select', value = ['none']) {
+    
+    const select = document.createElement('select');
+    select.id = id;
+
+    for(let index = 0; index <= value.length - 1; index++) {
+        const options = document.createElement('option');
+        options.value = value[index];
+        options.innerText = value[index];
+
+        select.insertAdjacentElement('beforeend', options);
+    }
+
+    return select;
+}
+
+window.addEventListener('load', () => {
+
+    //const inselect = createSelectElement('inselect', ['a', 'b', 'c']);
+    //document.querySelector('.content').insertAdjacentElement('afterbegin', inselect);
+    return;
+});
