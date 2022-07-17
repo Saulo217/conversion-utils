f30a6b67e58826d7444476b6fd4d775867dcbb69
Saulo <saulo@saulo.com>
2022-07-17 16:13:11 -0300

    Adição de novas medidas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function calculusLength(userInput, difference){
+function calculus(userInput, difference){
     
     return Number(`${userInput? userInput : 1}E${difference? difference : 0}`);
 }
@@ -13,10 +13,66 @@ function selectElements(...selectors) {
     return elements;
 }
 
+function pageLength() {
+
+    const values = ['mm', 'cm', 'dm', 'm', 'dam', 'hm', 'km'];
+    const { inselect, outselect } = selectElements('#inselect', '#outselect');
+
+    for(let index = 0; index < values.length - 1; index++) {
+        inselect.options[index].innerText = values[index];
+        inselect.options[index].value = values[index];
+        outselect.options[index].innerText = values[index];
+        outselect.options[index].value = values[index];
+    }
+    return;
+}
+
+function pageCapacity() {
+
+    const values = ['ml', 'cl', 'dl', 'l', 'dal', 'hl', 'kl'];
+    const { inselect, outselect } = selectElements('#inselect', '#outselect');
+
+    for(let index = 0; index < values.length - 1; index++) {
+        inselect.options[index].innerText = values[index];
+        inselect.options[index].value = values[index];
+        outselect.options[index].innerText = values[index];
+        outselect.options[index].value = values[index];
+    }
+    return;
+}
+
+function pageMass() {
+
+    const values = ['mg', 'cg', 'dg', 'g', 'dag', 'hg', 'kg'];
+    const { inselect, outselect } = selectElements('#inselect', '#outselect');
+
+    for(let index = 0; index < values.length - 1; index++) {
+        inselect.options[index].innerText = values[index];
+        inselect.options[index].value = values[index];
+        outselect.options[index].innerText = values[index];
+        outselect.options[index].value = values[index];
+    }
+    return;
+}
+
+function pageVolume() {
+
+    const values = ['mm3', 'cm3', 'dm3', 'm3', 'dam3', 'hm3', 'km3'];
+    const { inselect, outselect } = selectElements('#inselect', '#outselect');
+
+    for(let index = 0; index < values.length - 1; index++) {
+        inselect.options[index].innerText = values[index];
+        inselect.options[index].value = values[index];
+        outselect.options[index].innerText = values[index];
+        outselect.options[index].value = values[index];
+    }
+    return;
+}
+
 function convert() {
     
     const { inselect, outselect, output, input } = selectElements('#inselect', '#outselect', '#output', '#input');
-    output.innerText = `Resultado: ${calculusLength(
+    output.innerText = `Resultado: ${calculus(
         input.value, inselect.selectedIndex - outselect.selectedIndex
     )} `;
 
