30353c38ffc88835726e54df5058e520a733a7a9
Saulo <saulo@saulo.com>
2022-07-20 12:44:21 -0300

    Ajustes na troca de medidas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function calculus(userInput, difference){
-    
+
     return Number(`${userInput? userInput : 1}E${difference? difference : 0}`);
 }
 
@@ -13,28 +13,8 @@ function selectElements(...selectors) {
     return elements;
 }
 
-function pages(...values) {
-
-    function update(select, value) {
-
-        for(let index = 0; index < value.length - 1; index++) {
-
-            select.options[index].innerText = value[index];
-            select.options[index].value = value[index];
-        }
-
-        return;
-    }
-    
-    const { inselect, outselect } = selectElements('#inselect', '#outselect');
-
-    update(inselect, values);
-    update(outselect, values);
-    return;
-}
-
 function convert() {
-    
+
     const { inselect, outselect, output, input } = selectElements('#inselect', '#outselect', '#output', '#input');
     output.innerText = `Resultado: ${calculus(
         input.value, inselect.selectedIndex - outselect.selectedIndex
@@ -43,18 +23,37 @@ function convert() {
     return;
 }
 
-window.addEventListener('load', () => {
+function pages(select, value) {
 
-    document.querySelector('.pageLength').addEventListener('click', () => {
-        pages('mm', 'cm', 'dm', 'm', 'dam', 'hm', 'km');
-    });
-    document.querySelector('.pageCapacity').addEventListener('click', () => {
-        pages('ml', 'cl', 'dl', 'l', 'dal', 'hl', 'kl');
-    });
-    document.querySelector('.pageMass').addEventListener('click', () => {
-        pages('mg', 'cg', 'dg', 'g', 'dag', 'hg', 'kg');
-    });
-    document.querySelector('.pageVolume').addEventListener('click', () => {
-        pages('mm3', 'cm3', 'dm3', 'm3', 'dam3', 'hm3', 'km3');
-    });
+
+    for(let index = 0; index < value.length; index++) {
+
+        select.options[index].innerText = value[index];
+        select.options[index].value = value[index];
+    }
+
+    return;
+}
+
+const VALUES = {
+    length: ['mm', 'cm', 'dm', 'm', 'dam', 'hm', 'km'],
+    capacity: ['ml', 'cl', 'dl', 'l', 'dal', 'hl', 'kl'],
+    mass: ['mg', 'cg', 'dg', 'g', 'dag', 'hg', 'kg'],
+    volume: ['mm3', 'cm3', 'dm3', 'm3', 'dam3', 'hm3', 'km3']
+};
+
+const { inputType, /*outputType*/ } = selectElements('.inputType', '.outputType');
+const { inselect, outselect } = selectElements('#inselect', '#outselect');
+
+inputType.addEventListener('change', () => {
+
+    pages(inselect, VALUES[inputType.options[inputType.selectedIndex].value]);
+    pages(outselect, VALUES[inputType.options[inputType.selectedIndex].value]);
 });
+
+/*
+outputType.addEventListener('change', () => {
+
+    pages(outselect, VALUES[outputType.options[outputType.selectedIndex].value]);
+});
+*/
